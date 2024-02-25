f4202f888b25d7a52226221f14e6607eab305f13
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 18:54:59 +0000

    Update App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import './assets/styles/App.css';
+import { useState } from 'react';
+import FooterComponent from './components/FooterComponent';
+import Header from './components/HeaderComponent';
+import MainCanvas from './components/MainCanvas';
+import { motion} from 'framer-motion';
+
+const childVariantsR = {
+  hidden: { x: -270, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { delay: 0.2, duration: 1 } },
+};
+
+const childVariantsL = {
+  hidden: { x: 270, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { delay: 0.2, duration: 1 } },
+};
+
+const App = () => {  
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = (modeName: string) => {
+    setMode(modeName);
+  };
+
+  return (
+    <div className={`App ${mode}`}>
+        <div className={`HeaderContainer ${mode}`}>
+          <Header mode={mode} toggleMode={toggleMode} />
+        </div>
+        <motion.div 
+        className={`CanvasContainer ${mode}`}>
+          <motion.div variants={childVariantsR}  initial="hidden" whileInView={"visible"}>
+            <MainCanvas mode={mode} />
+          </motion.div>
+          <motion.div variants={childVariantsL}  initial="hidden" whileInView={"visible"}>
+            <MainCanvas mode={mode} />
+          </motion.div>
+        </motion.div>
+        <div className={`FooterContainer ${mode}`}>
+          <FooterComponent mode={mode} />
+        </div>
+    </div>
+  )
+}
+
+export default App

7a37f09db708946b926bf401257408161990d6fe
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 19:14:20 +0000

    Updated mainCell.ts

--- src/assets/cells/mainCell.ts
+++ src/assets/cells/mainCell.ts
@@ -1,3 +1,13 @@
+import mainEffect from "./mainEffect"
+
 export default class mainCell {
-    
+    effect: mainEffect;
+    x: number;
+    y: number;
+
+    constructor(mainEffect, x, y) {
+        this.effect = mainEffect;
+        this.x = x;
+        this.y = y;
+    }
 }

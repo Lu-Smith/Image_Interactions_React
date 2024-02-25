c183cdd3683c6c7d662c61af013f175a632ec2ca
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 19:23:28 +0000

    Updated mainEffect.ts

--- src/assets/cells/mainEffect.ts
+++ src/assets/cells/mainEffect.ts
@@ -8,13 +8,15 @@ export default class mainEffect {
     cellHeight: number;
     cell: mainCell;
 
- constructor(canvas) {
+ constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas;
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.cellWidth = this.width / 35;
     this.cellHeight = this. height / 55;
     this.cell = new mainCell(this, 0, 0);
-
+ }
+ render(context: CanvasRenderingContext2D) {
+    this.cell.draw(context);
  }
 }

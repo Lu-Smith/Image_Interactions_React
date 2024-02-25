d99fcd503904c84fd83a074a62cc28f734e3e12b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 19:04:55 +0000

    Created mainEffect.ts

---
+++ src/assets/cells/mainEffect.ts
@@ -0,0 +1,12 @@
+export default class mainEffect {
+    canvas: HTMLCanvasElement;
+    width: number;
+    height: number;
+
+ constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+
+ }
+}

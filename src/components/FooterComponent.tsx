96d3ed196bdf4b005851bad5f5a00b5cde39072f
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 18:55:42 +0000

    Created Footer.Component.tsx

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../assets/styles/Footer.css';
+
+interface FooterProps {
+  mode: string;  
+}
+
+const FooterComponent: React.FC<FooterProps> = ({mode}) => {
+  return (
+    <div className={`Footer ${mode}`}>
+        <h3>Coded by 
+            <a
+             href='https://www.lunasmithart.com/' 
+             target='_blank'
+             className='portfolio'>
+                Luna Smith
+            </a>
+            -
+            <a 
+            href='https://github.com/Lu-Smith/flow_fields_react'
+            target='_blank'>
+                GitHub
+            </a>
+            .
+        </h3>
+    </div>
+  )
+}
+
+export default FooterComponent

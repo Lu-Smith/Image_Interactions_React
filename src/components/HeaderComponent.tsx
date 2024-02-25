22d3f85c4ed93def7827505615942c802d2fff20
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-25 18:56:02 +0000

    Created HeaderComponent.tsx

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../assets/styles/Header.css';
+import { motion} from 'framer-motion';
+
+interface HeaderProps {
+  mode: string; 
+  toggleMode: (modeName: string) => void; 
+}
+
+const HeaderComponent: React.FC<HeaderProps> = ({mode, toggleMode}) => {
+  return (
+    <div className={`Header ${mode}`}>
+      <h1 className='title'>Image Interactions</h1>
+      <div>
+      <motion.div  whileHover={{ scale: 1.3 }} whileTap={{ scale: 1.1 }}>
+        <span className="material-symbols-outlined light" onClick={() => toggleMode('light')}>
+          wb_sunny
+        </span>
+      </motion.div>
+      <motion.div  whileHover={{ scale: 1.3 }} whileTap={{ scale: 1.1 }}>
+        <span className="material-symbols-outlined dark" onClick={() => toggleMode('dark')}>
+          nightlight
+        </span>
+      </motion.div>
+      <motion.div  whileHover={{ scale: 1.3 }} whileTap={{ scale: 1.1 }}>
+        <span className="material-symbols-outlined bolt" onClick={() => toggleMode('bolt')}>
+          bolt
+        </span>
+      </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderComponent

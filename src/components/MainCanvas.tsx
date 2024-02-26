f4dea89955986663f89ebe5e0744d466e51ac156
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-26 15:56:59 +0000

    Updated MainCanvas.tsx

--- src/components/MainCanvas.tsx
+++ src/components/MainCanvas.tsx
@@ -8,6 +8,10 @@ import mobileImage from '../assets/images/AlpsSmall.jpg';
 import effectOne from '../assets/cells/effectOne';
 import Image1 from '../assets/images/Garden.jpg';
 import mobileImage1 from '../assets/images/GardenSmall.jpg';
+//image3
+import effectTwo from '../assets/cells/effectTwo';
+import Image2 from '../assets/images/Guardian.jpg';
+import mobileImage2 from '../assets/images/GuardianSmall.jpg';
 
 interface MainProps {
   mode: string;
@@ -17,11 +21,14 @@ interface MainProps {
 const MainCanvas: React.FC<MainProps> = ({ mode, imageNumber }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const canvasRef1 = useRef<HTMLCanvasElement>(null);
+  const canvasRef2 = useRef<HTMLCanvasElement>(null);
   const [smallImage, setSmallImage] = useState<boolean>(false);
   const [smallImage1, setSmallImage1] = useState<boolean>(false);
+  const [smallImage2, setSmallImage2] = useState<boolean>(false);
 
   useEffect(() => {
-    const canvas = imageNumber === 1 ? canvasRef.current : canvasRef1.current;
+    const canvas = imageNumber === 1 ? canvasRef.current : 
+    imageNumber === 2 ? canvasRef1.current: canvasRef2.current;
     if (!canvas) return;
 
     const ctx = canvas.getContext('2d');
@@ -33,17 +40,20 @@ const MainCanvas: React.FC<MainProps> = ({ mode, imageNumber }) => {
         canvas.height = imageNumber === 1 ? 722 : 804; 
         setSmallImage(false);
         setSmallImage1(false);
+        setSmallImage2(false);
       } else {
         canvas.width = 300;
         canvas.height = imageNumber === 1 ? 361 : 402; 
         setSmallImage(true);
         setSmallImage1(true);
+        setSmallImage2(true);
       }
     };
 
     setCanvasDimensions();
 
-    const effect = imageNumber === 1 ? new mainEffect(canvas) : new effectOne(canvas);
+    const effect = imageNumber === 1 ? new mainEffect(canvas) : 
+    imageNumber === 2 ? new effectOne(canvas) : new effectTwo(canvas);
     
 
     const animate = () => {
@@ -62,7 +72,7 @@ const MainCanvas: React.FC<MainProps> = ({ mode, imageNumber }) => {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, [imageNumber]);
+  }, []);
 
   return (
     <div className={`MainCanvas ${mode}`}>
@@ -82,6 +92,15 @@ const MainCanvas: React.FC<MainProps> = ({ mode, imageNumber }) => {
             src={smallImage1 ? mobileImage1 : Image1}
             id="Image1" 
             alt="Secret Garden by Luna Smith" />
+        </div>
+      )}
+      {(imageNumber === 3) && (
+        <div>
+          <canvas ref={canvasRef2} id="canvas3"></canvas>
+          <img 
+            src={smallImage2 ? mobileImage2 : Image2}
+            id="Image2" 
+            alt="Gurdian Tree by Luna Smith" />
         </div>
       )}
     </div>

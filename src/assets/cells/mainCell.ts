e0e0579f123c2e48a0cb945127066ef653980d31
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-26 15:11:52 +0000

    Updated mainCell.ts

--- src/assets/cells/mainCell.ts
+++ src/assets/cells/mainCell.ts
@@ -13,11 +13,20 @@ export default class mainCell {
     vy: number;
     ease: number;
     friction: number;
+    positionX: number;
+    positionY: number;
+    speedX: number;
+    speedY: number;
+    randomize: number;
 
     constructor(effect: mainEffect, x: number, y: number) {
         this.effect = effect;
         this.x = x;
         this.y = y;
+        this.positionX = 0;
+        this.positionY = 0;
+        this.speedX = 0;
+        this.speedY = 0;
         this.width = this.effect.cellWidth;
         this.height = this.effect.cellHeight;
         this.image = document.getElementById('mainImage') as HTMLCanvasElement;
@@ -25,14 +34,22 @@ export default class mainCell {
         this.slideY = 0;
         this.vx = 0;
         this.vy = 0;
-        this.ease = 0.05;
-        this.friction = 0.8;
+        this.ease = 0.3;
+        this.friction = 0.9;
+        this.randomize = Math.random() * 50 + 2;
     }
     draw(context: CanvasRenderingContext2D) {
-        context.drawImage(this.image, this.x + this.slideX, this.y + this.slideY, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x + this.slideX, this.y + this.slideY, 
+            this.width, this.height, this.positionX, this.positionY, this.width, this.height);
         // context.strokeRect(this.x, this.y, this.width, this.height);
     }
     update() {
+        //cell position
+            this.speedX = (this.x - this.positionX)/this.randomize;
+            this.speedY = (this.y - this.positionY)/this.randomize;
+            this.positionX += this.speedX;
+            this.positionY += this.speedY;
+        //crop
         if (this.effect.mouse.x && this.effect.mouse.y) {
             const dx = this.effect.mouse.x - this.x;
             const dy = this.effect.mouse.y - this.y;

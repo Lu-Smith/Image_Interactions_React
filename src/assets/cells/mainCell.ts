19d7ceee91c5a346efa12ef55a4cb42a450b704d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-26 12:42:23 +0000

    Updated mainCell.ts

--- src/assets/cells/mainCell.ts
+++ src/assets/cells/mainCell.ts
@@ -9,6 +9,7 @@ export default class mainCell {
     image: CanvasImageSource;
     slideX: number;
     slideY: number;
+    vx: number;
 
     constructor(effect: mainEffect, x: number, y: number) {
         this.effect = effect;
@@ -18,13 +19,28 @@ export default class mainCell {
         this.height = this.effect.cellHeight;
         this.image = document.getElementById('mainImage') as HTMLCanvasElement;
         this.slideX = 0;
-        this.slideY = Math.random() * 2;
+        this.slideY = 0;
+        this.vx = 0;
     }
     draw(context: CanvasRenderingContext2D) {
         context.drawImage(this.image, this.x + this.slideX, this.y + this.slideY, this.width, this.height, this.x, this.y, this.width, this.height);
         context.strokeRect(this.x, this.y, this.width, this.height);
     }
     update() {
-        this.slideX += 1;
+        // if (this.effect.mouse.x && this.effect.mouse.y) {
+            const dx = this.effect.mouse.x - this.x;
+            const dy = this.effect.mouse.y - this.y;
+
+            const distance = Math.hypot(dx, dy);
+            if (distance < this.effect.mouse.radius) {
+                const force = distance / this.effect.mouse.radius;
+                this.vx = force;
+            } else {
+                this.vx = 0;
+            }
+            this.slideX += this.vx;
+            this.slideY += this.vx;
+         
+        // }
     }
 }

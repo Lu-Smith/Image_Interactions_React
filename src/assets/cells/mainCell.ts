dc5cf631770240c6f53562235f815a36be0f29d0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-26 12:15:47 +0000

    Updated mainCell.ts

--- src/assets/cells/mainCell.ts
+++ src/assets/cells/mainCell.ts
@@ -7,6 +7,8 @@ export default class mainCell {
     width: number;
     height: number;
     image: CanvasImageSource;
+    slideX: number;
+    slideY: number;
 
     constructor(effect: mainEffect, x: number, y: number) {
         this.effect = effect;
@@ -15,9 +17,14 @@ export default class mainCell {
         this.width = this.effect.cellWidth;
         this.height = this.effect.cellHeight;
         this.image = document.getElementById('mainImage') as HTMLCanvasElement;
+        this.slideX = 0;
+        this.slideY = Math.random() * 2;
     }
     draw(context: CanvasRenderingContext2D) {
-        context.drawImage(this.image, this.x, this.y, this.width, this.height, this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.x + this.slideX, this.y + this.slideY, this.width, this.height, this.x, this.y, this.width, this.height);
         context.strokeRect(this.x, this.y, this.width, this.height);
+    }
+    update() {
+        this.slideX += 1;
     }
 }

8986ee9cf48f2300c142ee05feca16a73546fac1
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-27 11:42:27 +0000

    Created effectThree.ts

---
+++ src/assets/cells/effectThree.ts
@@ -0,0 +1,51 @@
+import cellThree from "./cellThree";
+
+export default class effectThree {
+    canvas: HTMLCanvasElement;
+    width: number;
+    height: number;
+    cellWidth: number;
+    cellHeight: number;
+    imageGrid: cellThree[];
+    mouse: {x: number | undefined, y: number | undefined, radius: number};
+
+ constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.cellWidth = this.width / 15;
+    this.cellHeight = this. height / 35;
+    this.imageGrid = [];
+    this.createGrid();
+    this.mouse = {
+      x: undefined,
+      y: undefined,
+      radius: 80,
+    }
+    this.canvas.addEventListener('mousemove', e => {
+      this.mouse.x = e.offsetX;
+      this.mouse.y = e.offsetY;
+    })
+
+    this.canvas.addEventListener('mouseleave', () => {
+      this.mouse.x = undefined;
+      this.mouse.y = undefined;
+    })
+
+ }
+ createGrid() {
+   let index = 0;
+   for (let y = 0; y < this.height; y += this.cellHeight) {
+      for (let x = 0; x < this.width; x += this.cellWidth) {
+        index++;
+        this.imageGrid.unshift(new cellThree(this, x, y, index));
+      }
+   }
+ }
+ render(context: CanvasRenderingContext2D) {
+    this.imageGrid.forEach(cell => {
+      cell.update();
+      cell.draw(context);
+    })
+ }
+}
